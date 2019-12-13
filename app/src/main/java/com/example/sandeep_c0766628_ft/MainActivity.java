592f51e33b8d1241770d010b78b1990e2e7f16e6
Sandeep <sandeep.mahal6@gmail.com>
2019-12-13 08:58:46 -0500

    test

---
+++ app/src/main/java/com/example/sandeep_c0766628_ft/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sandeep_c0766628_ft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
